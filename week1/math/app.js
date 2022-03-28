2d6502d15cd7409b9db8beec4b42957d45cf64b0
Colton Freeman <91649148+coltonfreeman@users.noreply.github.com>
2022-03-28 00:14:17 -0500

    added assignment

---
+++ week1/math/app.js
@@ -0,0 +1,10 @@
+
+const mathObj = require('./math.js');
+
+console.log(mathObj.add(1, 2, 3)); 
+
+console.log(mathObj.subtract(1, 2, 3));
+
+console.log(mathObj.multiply(4, 5, 6));
+
+console.log(mathObj.divide(4, 5, 6));
